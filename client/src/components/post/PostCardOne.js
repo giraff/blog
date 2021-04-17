42c6484d7a823270a0ef42eca9b71621d541bcef
giraff <hse05105@naver.com>
2021-04-17 14:57:49 +0900

    console 지우기, postCardOne 에러 수정

--- client/src/components/post/PostCardOne.js
+++ client/src/components/post/PostCardOne.js
@@ -8,24 +8,24 @@ const PostCardOne = ({posts}) => {
   return(
     <>
     {
-      Array.isArray(posts) ? posts.map(({_id, title, fileUrl, comments, view}) => {
+      Array.isArray(posts) ? posts.map((post) => {
         return (
-          <div key={_id} className="col-md-4">
-            <Link to={`/post/${_id}`} className="text-dark text-decoration-none">
+          <div key={post._id} className="col-md-4">
+            <Link to={`/post/${post._id}`} className="text-dark text-decoration-none">
               <Card>
-                <CardImg top alt="card image" src={fileUrl}/>
+                <CardImg top alt="card image" src={post.fileUrl}/>
                 <CardBody>
                   <CardTitle className="text-truncate d-flex justify-content-between">
-                    <span className="text-truncate">{title}</span>
+                    <span className="text-truncate">{post.title}</span>
                     <span>
                       <FontAwesomeIcon icon={faMouse} />
                       &nbsp;&nbsp;
-                      <span>{view}</span>
+                      <span>{post.view}</span>
                     </span>
                   </CardTitle>
                   <Row>
                     <Button color="warning" className="p-1 btn-block">
-                      Comments <Badge color="light">{comments ? comments.length : 0}</Badge>
+                      Comments <Badge color="light">{post.comments ? post.comments.length : 0}</Badge>
                     </Button>
                   </Row>
                 </CardBody>
